7483db0fc51c41e540a8f712a6184bb06b8a7812
SirTurboPascal <theogernke@icloud.com>
2021-12-26 08:35:40 +0100

    Adds user id and display name to the UserSection component

--- src/components/UserSection/UserSection.tsx
+++ src/components/UserSection/UserSection.tsx
@@ -17,7 +17,7 @@ const StyledUserSection = styled.footer`
 	background-color: #ebedef;
 
 	& > * {
-		margin-right: 4px;
+		margin-right: 8px;
 
 		&:last-child {
 			margin-right: 0;
@@ -29,16 +29,67 @@ const StyledUserSection = styled.footer`
 	}
 `;
 
+const StyledUserContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex-grow: 1;
+	justify-content: stretch;
+
+	overflow: hidden;
+	cursor: pointer;
+	user-select: none;
+`;
+
+const StyledUserId = styled.span`
+	color: #4f5660;
+	overflow: hidden;
+	text-overflow: ellipsis;
+
+	font-size: 12px;
+	font-weight: 600;
+
+	line-height: 12px;
+	white-space: nowrap;
+
+	@media (prefers-color-scheme: dark) {
+		color: #babbbe;
+	}
+`;
+
+const StyledUserName = styled.h4`
+	overflow: hidden;
+	text-overflow: ellipsis;
+
+	font-size: 14px;
+	font-weight: 700;
+
+	line-height: 14px;
+	white-space: nowrap;
+`;
+
 interface UserSectionProps {
 	user: IUser;
 }
 
 const UserSection: FunctionComponent<UserSectionProps> = (props) => {
+	const handleUserContainerClick = () => {
+		navigator.clipboard.writeText(props.user.uid);
+	};
+
 	return (
 		<>
 			<StyledUserSection>
 				<Avatar photoUrl={props.user.photoUrl} />
-				<div style={{ flexGrow: 1 }} />
+
+				<StyledUserContainer onClick={handleUserContainerClick}>
+					{props.user.displayName && (
+						<>
+							<StyledUserName>{props.user.displayName}</StyledUserName>
+						</>
+					)}
+
+					<StyledUserId>#{props.user.uid}</StyledUserId>
+				</StyledUserContainer>
 
 				{props.children}
 			</StyledUserSection>
